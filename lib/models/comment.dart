ac9153e74734fae3b65b7bf8eac5eb32e7c8064b
Marcin Warczyk <marcin.warczyk@gmail.com>
2022-09-02 08:07:47 +0200

    models added

---
+++ lib/models/comment.dart
@@ -0,0 +1,23 @@
+class Comment {
+  int postId;
+  int id;
+  String name;
+  String email;
+  String body;
+
+  Comment({
+    required this.postId,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.body,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "postId": postId,
+        "id": id,
+        "name": name,
+        "email": email,
+        "body": body,
+      };
+}
---
+++ lib/models/photo.dart
@@ -0,0 +1,23 @@
+class Photo {
+  int albumId;
+  int id;
+  String title;
+  String url;
+  String thumbnailUrl;
+
+  Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  Map<String, dynamic> toJson() => {
+    "albumId": albumId,
+    "id": id,
+    "title": title,
+    "url": url,
+    "thumbnailUrl": thumbnailUrl,
+  };
+}
